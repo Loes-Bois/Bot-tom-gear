38bd221317d948cdf1f9e09b5098568f007c70a0
Spilkey <spilkey1234@gmail.com>
2020-10-15 21:35:18 -0400

    command constants

---
+++ models/tags_commands.py
@@ -0,0 +1,11 @@
+import discord
+def HELP():
+	HELP = discord.Embed(
+		title="Hello I am Embed",
+		colour=discord.Colour(0xE5E242),
+		url=f"https://www.kingsmathsschool.com/weekly-maths-challenge/",
+		description="description",
+		thumbnail="https://pbs.twimg.com/profile_images/502115424121528320/hTQzj_-R.png",
+		author="asdfasdfasdfa"
+		)
+	return HELP
