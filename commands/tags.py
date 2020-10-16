b2a159dc071b3e6afc1b27a2b71051103738a7c1
Spilkey <spilkey1234@gmail.com>
2020-10-15 22:41:07 -0400

    tag command file

--- commands/tags.py
+++ commands/tags.py
@@ -1,21 +1,52 @@
 from models import database, tags_commands
 
 async def main(self, message):
+    
+    # removing hashtag, could probably combine these two lines
+    tag_command = message.content.split(' ', 1)[1]
 
-    db = database.DBClient().get_db()
-    client = database.DBClient().get_client()
-    tag_collection = db.Tags2020
+    command = tag_command.split(' ', 1)[1] if len(tag_command.split(' ')) > 1 else " "
 
-    print(tag_collection)
-    result = tag_collection.find_one({"tag": "First Tag"})
+    result, embed = handle_tag_commands(command, message)
 
-    print(result)
-    content = result['content']
+    # returning embed if error or help
+    if not embed:   
+        await message.channel.send(result)
+    else:
+        await message.channel.send(embed=embed)  
+        
+    
 
 
-    await message.channel.send(embed=tags_commands.HELP())
+def handle_tag_commands(command, message):
+    """
+    Handles the different types of tag commands
+    command - the specific command if any the user wants for the tag
+    message - the user's message object for populating specific tag details in db
+    """
+    embed  = None
+    result = None
 
+    if(command == "create"):
+        # do create tings
+        print("do create")
+    elif(command == "help"):
+        embed = tags_commands.help()
+    elif(command == " "):
+        embed = tags_commands.nocommand()
+    else:
+        # Get our db instance
+        db = database.DBClient().get_db()
+        tag_collection = db.Tags2020
 
-def handle_tag_commands(message):
-    print()
+        # search for tag
+        content = tag_collection.find_one({"tag": command})
+
+        # if we have a result return the tag contents else show error
+        if(content):
+            result = content['content']
+        else:
+            embed = tags_commands.notagfound()
+    return result, embed
+        
 
